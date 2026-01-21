2d6453dd7f5320dbb859e717f7bf7703cb01abb3
ramzYka <ybloko2003god@gmail.com>
2026-01-21 18:08:19 +0300

    213

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -89,7 +89,7 @@
             console.log('Отфильтрованные элементы:', filteredItems.length);
             items.value = filteredItems.map((obj) => ({
                 ...obj,
-                imageUrl: `/sneakers/${obj.imageUrl.replace('/sneakers/', '')}`,  // Извлекает имя файла и добавляет /sneakers/
+                imageUrl: obj.imageUrl,  // Извлекает имя файла и добавляет /sneakers/
                 isFavorite: false,
                 favoriteId: null,
                 isAdded: false
